beadffe83223f5353297092fe65e5e49220e9d02
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-02-12 01:36:04 +0100

    changed extension name to vsx0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,14 @@
 # Change Log
 
-All notable changes to the "x0" extension will be documented in this file.
+All notable changes to the "vsx0" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
 ## [Unreleased]
 
-- Initial release
+### Added
+
+- Quick Pick Input for namespace when running the paste command
+- Also option to register new namespace on paste command
+
+- When you have no namespace registered yet, you will be asked whether you want to register a new one or submit and save an existing one
--- src/extension.ts
+++ src/extension.ts
@@ -15,13 +15,13 @@ const msg = {
 export async function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "x0" is now active!');
+  console.log('Congratulations, your extension "vsx0" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const paste_disposable = vscode.commands.registerCommand(
-    'x0.paste',
+    'vsx0.paste',
     async () => {
       // The code you place here will be executed every time your command is executed
 
